724a20ca300233bd99c90f7e232a36454185ecb8
Richard Myatt <lydgate77@hotmail.co.uk>
2017-09-25 12:20:25 +0100

    various small changes made - now good to go

--- js/css_shapes.js
+++ js/css_shapes.js
@@ -1,7 +1,19 @@
+/* Code activated by info button designed to show and information panel
+   previously hidden throught the use of 'display: none;' in the css code.
+   Requires a reference to material-icons in the head of the html file i.e.
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+           rel="stylesheet">
+   together with a reference to this script.
+*/
+
+// Set initial state of the information panel
 var infoVisible = false;
 
+/* This function is activated by an onclick event on the info icon setting the
+   state of the display to true or false and the visibility of the panel through
+   the 'display' attribute */
 function showInfo() {
-  var elem = document.getElementById('description-text');
+  var elem = document.getElementById('information-text');
   if(infoVisible) {
     elem.style.display = 'none';
     infoVisible = false;
